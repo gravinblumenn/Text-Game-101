83cdb2c7ad71ad7361c90e3cf1302fcd4ed8846c
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-21 13:46:50 -0600

    Initial Commit
    Choose your own pathtext adventure

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    
+   [SerializeField] Text textComponent;
+   [SerializeField] State startingState;
+
+   State state;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+    private void ManageState()
+    {
+        //var can be used if the variable declared and initialized
+        //can substiture var for State[]
+        var nextStates = state.GetNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+            else if(Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                state = startingState;
+            }
+        }
+        //else if (Input.GetKeyDown(KeyCode.))
+        textComponent.text = state.GetStateStory();
+    }
+}
